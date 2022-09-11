40655477b6f91e3d618c8f49ad225ff74d370067
Alex Greif <alex.greif@gmail.com>
2022-09-11 15:50:46 +0000

    added uikit assets

--- hive_rec/assets/js/app.js
+++ hive_rec/assets/js/app.js
@@ -1,6 +1,8 @@
 // We import the CSS which is extracted to its own file by esbuild.
 // Remove this line if you add a your own CSS build pipeline (e.g postcss).
 import "../css/app.css"
+import "../css/uikit.min.css"
+import "../css/uikit-rtl.min.css"
 
 // If you want to use Phoenix channels, run `mix help phx.gen.channel`
 // to get started and then uncomment the line below.
@@ -25,6 +27,8 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
+import uikit_min from "../vendor/uikit.min"
+import uikit_icons_min from "../vendor/uikit-icons.min"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
