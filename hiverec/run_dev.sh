cb75750d95a62b3a515f4e59f03bf04010cf922c
Alex Greif <alex.greif@gmail.com>
2022-10-16 12:29:54 +0200

    added other po files

--- hiverec/run_dev.sh
+++ hiverec/run_dev.sh
@@ -1,6 +1,9 @@
 rm priv/static/riot/*.js
 ./node_modules/.bin/riot priv/riot -o priv/static/riot/
 
+
+# mix gettext.merge priv/gettext --locale de
+
 mix gettext.extract
 mix gettext.merge priv/gettext
 
