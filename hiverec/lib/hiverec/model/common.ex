0b7ec639840cb996978c978439e8b40435935b03
Alex Greif <alex.greif@gmail.com>
2022-10-20 12:44:53 +0200

    minor fixes

--- hiverec/lib/hiverec/model/common.ex
+++ hiverec/lib/hiverec/model/common.ex
@@ -8,7 +8,7 @@ defmodule Hiverec.Model.Common do
   def validate_required_with_change(changeset, field) when is_atom(field) do
     validate_change(changeset, field, fn field, value ->
       case String.trim(value) do
-        "" -> [{field, "can't be empty"}]
+        "" -> [{field, "can't be blank"}]
         _ -> []
       end
     end)
