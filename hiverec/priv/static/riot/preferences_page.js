a6082134dddb94f2d475f9403f4b458b20fcfa90
Alex Greif <alex.greif@gmail.com>
2022-11-20 14:28:46 +0100

    inspection param removed sort indec column

--- hiverec/priv/static/riot/preferences_page.js
+++ hiverec/priv/static/riot/preferences_page.js
@@ -21,7 +21,7 @@ var preferences_page = {
       this.context.refreshDataPost(item.post_insparamtype_sortposdown_data_url, item.csrf_token);
     }
   },
-  template: (template, expressionTypes, bindingTypes, getComponent) => template('<h2 expr0="expr0"> </h2><div><div uk-grid><div class="uk-width-auto@m"><ul class="uk-tab-left" uk-tab="connect: #component-tab-left; animation: uk-animation-fast"><li><a expr1="expr1" href="#"> </a></li><li><a href="#">Item</a></li><li><a href="#">Item</a></li></ul></div><div class="uk-width-expand@m"><ul id="component-tab-left" class="uk-switcher"><li><table class="uk-table uk-table-small uk-table-divider uk-table-hover uk-background-muted"><thead><tr><th expr2="expr2" class> </th><th expr3="expr3" class> </th><th expr4="expr4" class> </th><th expr5="expr5" class> </th><th class="uk-align-right"><button expr6="expr6" class="uk-background-primary uk-light uk-padding-small modal-form-link uk-button uk-button-link" uk-tooltip><span uk-icon="icon: plus"></span></button></th></tr></thead><tbody><tr expr7="expr7"></tr></tbody></table><div expr17="expr17" uk-modal></div></li><li>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li><li>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur, sed do eiusmod.</li></ul></div></div></div>', [{
+  template: (template, expressionTypes, bindingTypes, getComponent) => template('<h2 expr0="expr0"> </h2><div><div uk-grid><div class="uk-width-auto@m"><ul class="uk-tab-left" uk-tab="connect: #component-tab-left; animation: uk-animation-fast"><li><a expr1="expr1" href="#"> </a></li><li><a href="#">Item</a></li><li><a href="#">Item</a></li></ul></div><div class="uk-width-expand@m"><ul id="component-tab-left" class="uk-switcher"><li><table class="uk-table uk-table-small uk-table-divider uk-table-hover uk-background-muted"><thead><tr><th expr2="expr2" class> </th><th expr3="expr3" class> </th><th expr4="expr4" class> </th><th class="uk-align-right"><button expr5="expr5" class="uk-background-primary uk-light uk-padding-small modal-form-link uk-button uk-button-link" uk-tooltip><span uk-icon="icon: plus"></span></button></th></tr></thead><tbody><tr expr6="expr6"></tr></tbody></table><div expr15="expr15" uk-modal></div></li><li>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</li><li>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur, sed do eiusmod.</li></ul></div></div></div>', [{
     redundantAttribute: 'expr0',
     selector: '[expr0]',
     expressions: [{
@@ -65,14 +65,6 @@ var preferences_page = {
     redundantAttribute: 'expr5',
     selector: '[expr5]',
     expressions: [{
-      type: expressionTypes.TEXT,
-      childNodeIndex: 0,
-      evaluate: _scope => _scope.context.data.translations['Sort Index']
-    }]
-  }, {
-    redundantAttribute: 'expr6',
-    selector: '[expr6]',
-    expressions: [{
       type: expressionTypes.EVENT,
       name: 'onclick',
       evaluate: _scope => _scope.addInsparamtype
@@ -85,9 +77,9 @@ var preferences_page = {
     type: bindingTypes.EACH,
     getKey: null,
     condition: null,
-    template: template('<td class="uk-table-link"><a expr8="expr8" class="uk-link-reset"> </a></td><td class="uk-table-link"><a expr9="expr9" class="uk-link-reset"> </a></td><td class="uk-table-link"><a expr10="expr10" class="uk-link-reset"><span expr11="expr11" class="textarea-wrap"> </span></a></td><td class="uk-table-link"><a expr12="expr12" class="uk-link-reset"> </a></td><td class="uk-width-small"><button expr13="expr13" class="uk-float-right uk-button uk-button-link" uk-tooltip><span uk-icon="icon: trash"></span></button><button expr14="expr14" class="uk-float-right uk-button uk-button-link" uk-tooltip><span uk-icon="icon: pencil"></span></button><button expr15="expr15" class="uk-float-right uk-button uk-button-link" uk-tooltip><span uk-icon="icon: arrow-down"></span></button><button expr16="expr16" class="uk-float-right uk-button uk-button-link" uk-tooltip><span uk-icon="icon: arrow-up"></span></button></td>', [{
-      redundantAttribute: 'expr8',
-      selector: '[expr8]',
+    template: template('<td class="uk-table-link"><a expr7="expr7" class="uk-link-reset"> </a></td><td class="uk-table-link"><a expr8="expr8" class="uk-link-reset"> </a></td><td class="uk-table-link"><a expr9="expr9" class="uk-link-reset"><span expr10="expr10" class="textarea-wrap"> </span></a></td><td class="uk-width-small"><button expr11="expr11" class="uk-float-right uk-button uk-button-link" uk-tooltip><span uk-icon="icon: trash"></span></button><button expr12="expr12" class="uk-float-right uk-button uk-button-link" uk-tooltip><span uk-icon="icon: pencil"></span></button><button expr13="expr13" class="uk-float-right uk-button uk-button-link" uk-tooltip><span uk-icon="icon: arrow-down"></span></button><button expr14="expr14" class="uk-float-right uk-button uk-button-link" uk-tooltip><span uk-icon="icon: arrow-up"></span></button></td>', [{
+      redundantAttribute: 'expr7',
+      selector: '[expr7]',
       expressions: [{
         type: expressionTypes.TEXT,
         childNodeIndex: 0,
@@ -98,8 +90,8 @@ var preferences_page = {
         evaluate: _scope => e => _scope.editInsparamtype(e, _scope.item)
       }]
     }, {
-      redundantAttribute: 'expr9',
-      selector: '[expr9]',
+      redundantAttribute: 'expr8',
+      selector: '[expr8]',
       expressions: [{
         type: expressionTypes.TEXT,
         childNodeIndex: 0,
@@ -110,36 +102,24 @@ var preferences_page = {
         evaluate: _scope => e => _scope.editInsparamtype(e, _scope.item)
       }]
     }, {
-      redundantAttribute: 'expr10',
-      selector: '[expr10]',
+      redundantAttribute: 'expr9',
+      selector: '[expr9]',
       expressions: [{
         type: expressionTypes.EVENT,
         name: 'onclick',
         evaluate: _scope => e => _scope.editInsparamtype(e, _scope.item)
       }]
     }, {
-      redundantAttribute: 'expr11',
-      selector: '[expr11]',
+      redundantAttribute: 'expr10',
+      selector: '[expr10]',
       expressions: [{
         type: expressionTypes.TEXT,
         childNodeIndex: 0,
         evaluate: _scope => _scope.item.insparamtype.options ? _scope.item.insparamtype.options.values.join('\n') : ""
       }]
     }, {
-      redundantAttribute: 'expr12',
-      selector: '[expr12]',
-      expressions: [{
-        type: expressionTypes.TEXT,
-        childNodeIndex: 0,
-        evaluate: _scope => _scope.item.insparamtype.sort_index
-      }, {
-        type: expressionTypes.EVENT,
-        name: 'onclick',
-        evaluate: _scope => e => _scope.editInsparamtype(e, _scope.item)
-      }]
-    }, {
-      redundantAttribute: 'expr13',
-      selector: '[expr13]',
+      redundantAttribute: 'expr11',
+      selector: '[expr11]',
       expressions: [{
         type: expressionTypes.ATTRIBUTE,
         name: 'uk-toggle',
@@ -150,8 +130,8 @@ var preferences_page = {
         evaluate: _scope => _scope.context.data.translations['Delete Inspection Param']
       }]
     }, {
-      redundantAttribute: 'expr14',
-      selector: '[expr14]',
+      redundantAttribute: 'expr12',
+      selector: '[expr12]',
       expressions: [{
         type: expressionTypes.EVENT,
         name: 'onclick',
@@ -162,8 +142,8 @@ var preferences_page = {
         evaluate: _scope => _scope.context.data.translations['Edit Inspection Param']
       }]
     }, {
-      redundantAttribute: 'expr15',
-      selector: '[expr15]',
+      redundantAttribute: 'expr13',
+      selector: '[expr13]',
       expressions: [{
         type: expressionTypes.EVENT,
         name: 'onclick',
@@ -174,8 +154,8 @@ var preferences_page = {
         evaluate: _scope => _scope.context.data.translations['Move Sort Position Down']
       }]
     }, {
-      redundantAttribute: 'expr16',
-      selector: '[expr16]',
+      redundantAttribute: 'expr14',
+      selector: '[expr14]',
       expressions: [{
         type: expressionTypes.EVENT,
         name: 'onclick',
@@ -186,8 +166,8 @@ var preferences_page = {
         evaluate: _scope => _scope.context.data.translations['Move Sort Position Up']
       }]
     }]),
-    redundantAttribute: 'expr7',
-    selector: '[expr7]',
+    redundantAttribute: 'expr6',
+    selector: '[expr6]',
     itemName: 'item',
     indexName: null,
     evaluate: _scope => _scope.context.data.pages.preferences.insparamtype_list_items
@@ -195,11 +175,27 @@ var preferences_page = {
     type: bindingTypes.EACH,
     getKey: null,
     condition: null,
-    template: template('<div class="uk-modal-dialog uk-modal-body"><h2 expr18="expr18" class="uk-modal-title"> </h2><p expr19="expr19"> </p><p class="uk-text-center"><button expr20="expr20" class="uk-button uk-button-default uk-modal-close"> </button><button expr21="expr21" class="uk-button uk-button-primary uk-modal-close"> </button></p></div>', [{
+    template: template('<div class="uk-modal-dialog uk-modal-body"><h2 expr16="expr16" class="uk-modal-title"> </h2><p expr17="expr17"> </p><p class="uk-text-center"><button expr18="expr18" class="uk-button uk-button-default uk-modal-close"> </button><button expr19="expr19" class="uk-button uk-button-primary uk-modal-close"> </button></p></div>', [{
       expressions: [{
         type: expressionTypes.ATTRIBUTE,
         name: 'id',
         evaluate: _scope => ['modal-insparamtype-really-delete-', _scope.item.insparamtype.id].join('')
+      }]
+    }, {
+      redundantAttribute: 'expr16',
+      selector: '[expr16]',
+      expressions: [{
+        type: expressionTypes.TEXT,
+        childNodeIndex: 0,
+        evaluate: _scope => _scope.context.data.translations['Delete Inspection Param']
+      }]
+    }, {
+      redundantAttribute: 'expr17',
+      selector: '[expr17]',
+      expressions: [{
+        type: expressionTypes.TEXT,
+        childNodeIndex: 0,
+        evaluate: _scope => _scope.context.data.translations['Do you really want to delete this Inspection Param?']
       }]
     }, {
       redundantAttribute: 'expr18',
@@ -207,27 +203,11 @@ var preferences_page = {
       expressions: [{
         type: expressionTypes.TEXT,
         childNodeIndex: 0,
-        evaluate: _scope => _scope.context.data.translations['Delete Inspection Param']
+        evaluate: _scope => [_scope.context.data.translations['Cancel']].join('')
       }]
     }, {
       redundantAttribute: 'expr19',
       selector: '[expr19]',
-      expressions: [{
-        type: expressionTypes.TEXT,
-        childNodeIndex: 0,
-        evaluate: _scope => _scope.context.data.translations['Do you really want to delete this Inspection Param?']
-      }]
-    }, {
-      redundantAttribute: 'expr20',
-      selector: '[expr20]',
-      expressions: [{
-        type: expressionTypes.TEXT,
-        childNodeIndex: 0,
-        evaluate: _scope => [_scope.context.data.translations['Cancel']].join('')
-      }]
-    }, {
-      redundantAttribute: 'expr21',
-      selector: '[expr21]',
       expressions: [{
         type: expressionTypes.TEXT,
         childNodeIndex: 0,
@@ -238,8 +218,8 @@ var preferences_page = {
         evaluate: _scope => e => _scope.deleteInsparamtype(e, _scope.item)
       }]
     }]),
-    redundantAttribute: 'expr17',
-    selector: '[expr17]',
+    redundantAttribute: 'expr15',
+    selector: '[expr15]',
     itemName: 'item',
     indexName: null,
     evaluate: _scope => _scope.context.data.pages.preferences.insparamtype_list_items
