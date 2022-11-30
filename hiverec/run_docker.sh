37527d1b7653da5bbcad61844fcfa8d41ca87c13
Alex Greif <alex.greif@gmail.com>
2022-11-30 12:45:15 +0100

    fixed endpoint for localhost:4000

--- hiverec/run_docker.sh
+++ hiverec/run_docker.sh
@@ -3,15 +3,17 @@ export SECRET_KEY_BASE=oxTDjL8cnfjaGQ2jEKC1S+S6d9HEDpdfan9RW1+7iWAljH7L1er550blp
 export PHX_HOST=localhost
 export PHX_PORT=4000
 export PHX_SCHEME=http
+export PORT=4000
 
 docker rm -f hiverec \
     && docker image build -t elixir/hiverec . \
-    && docker run -dp 4000:4000 \
+    && docker run -dp $PORT:$PORT \
 	      -e DATABASE_URL \
 	      -e SECRET_KEY_BASE \
 	      -e PHX_HOST \
 	      -e PHX_PORT \
 	      -e PHX_SCHEME \
+	      -e PORT \
 	      --network hiverec-network \
 	      --name hiverec \
 	      elixir/hiverec
